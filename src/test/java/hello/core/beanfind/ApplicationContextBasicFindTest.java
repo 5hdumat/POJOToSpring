293a93056e39e19714ac18a7a091cf0e9ce1d365
5hdumatt <5hdumatt@gmail.com>
2021-10-16 03:57:04 +0900

    스프링 컨테이너, 스프링 빈 조회, 싱글톤 컨테이너, @Configuration 코드 정리

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,48 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회하기")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름 없이 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회하기")
+    void findBeanByName2() {
+        // getBean()은 스프링 빈에 등록되어있는 인스턴스 타입을 보고 결정하기 때문에 구체 클래스로 기재해도 된다.
+        // 하지만 항상 역할에 의존해야지 구체에 의존하면 안된다. 그러므로 추천하는 방법은 아니다.
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 안되는 상황 만들기")
+    void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("XXXXX", MemberService.class));
+    }
+}
---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,56 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA: A 사용자가 10000원 주문
+        statefulService1.statefulOrder("userA", 10000);
+        // ThreadB: A 사용자가 20000원 주문
+        statefulService2.statefulOrder("userB", 20000);
+
+        // ThreadA: A 사용자 주문 금액 조회
+        int price = statefulService1.getPrice();
+        // ThreadA: 사용자A는 10000원을 기대했지만, 기대와 다르게 20000원 출력
+        System.out.println("price = " + price);
+
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    @Test
+    void statelessServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA: A 사용자가 10000원 주문
+        int userAPrice = statefulService1.statelessOrder("userA", 10000);
+        // ThreadB: A 사용자가 20000원 주문
+        int userBPrice = statefulService2.statelessOrder("userB", 20000);
+
+        // ThreadA: 사용자A가 구매한 금액이 그대로 조회된다.
+        System.out.println("price = " + userAPrice);
+
+        assertThat(userAPrice).isEqualTo(10000);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
